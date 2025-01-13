cef834489e537817ca69b72da4e90591224aa4eb
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-01-13 09:04:15 +0530

    Add files via upload

---
+++ Back-End/Node JS/NPM/index.js
@@ -0,0 +1,17 @@
+
+//var generatename = require('sillyname');
+/*import generateName from 'sillyname';
+var SillyName=generateName();
+console.log(`MY name is ${SillyName}.`); */
+
+
+import { randomSuperhero } from 'superheroes';
+
+var name=randomSuperhero()
+console.log(`My Favorite Superhero is ${name}.`); 
+
+/*import superheroes from "superheroes";
+
+const superheroesNames = superheroes.random();
+
+console.log(superheroesNames); */ 
